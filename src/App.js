2683c9f70db9e423947e10141b651d3da6cd71eb
Bruno N. Campos <bruno@mambowifi.com>
2021-04-14 17:26:42 -0300

    Using hooks but has errors

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Button, Form, Container, Header } from 'semantic-ui-react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [salary, setSalary] = useState('');
+  const [hobby, setHobby] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const objt = { name, age, salary, hobby }
+
+    axios.post('https://sheet.best/api/sheets/19142baf-a9b9-44e8-a109-c2ee3bbe8ffb', objt).then((response) => {
+      console.log(response);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid className="container">
+      <Header as="h2">React Google Sheets!</Header>
+      <Form className="form">
+        <Form.Field>
+          <label>Name</label>
+          <input 
+            placeholder="Enter your name"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </Form.Field>
+
+        <Form.Field>
+          <label>Age</label>
+          <input
+            placeholder='Enter your age'
+            onChange={(e) => setAge(e.target.value)}
+          />
+        </Form.Field>
+
+        <Form.Field>
+          <label>Salary</label>
+          <input
+            placeholder='Enter your salary'
+            onChange={(e) => setSalary(e.target.value)}
+          />
+        </Form.Field>
+
+        <Form.Field>
+          <label>Hobby</label>
+          <input
+            placeholder='Enter your hobby'
+            onChange={(e) => setHobby(e.target.value)}
+          />
+        </Form.Field>
+        
+        <Button color="blue" type='submit' onClick={handleSubmit}>Submit</Button>
+      </Form>
+    </Container>
   );
 }
 
